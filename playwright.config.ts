019d702dc0bf3fec1ec59515522489f8274282cf
Yogesh Wankhede <yogesh.wankhede@syngenta.com>
2025-03-26 17:23:30 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]], // Saves report in 'playwright-report'
   use: {
-    headless: false, // Run in headless mode
+    headless: true, // Run in headless mode
     screenshot: 'on', // Capture screenshots on failures
     video: 'on', // Record video of the test run
     trace: 'on', // Capture execution trace
