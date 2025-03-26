a4d04476584a6d124bc5195ac383d91e90b8b0a8
Yogesh Wankhede <yogesh.wankhede@syngenta.com>
2025-03-26 23:14:09 +0530

    Update playwright.config.ts
    useragents added

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  timeout: 30000, // Set global timeout for tests
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]], // Saves report in 'playwright-report'
   use: {
-    headless: false, // Run in headless mode
+    headless: true, // Run in non-headless mode
     screenshot: 'on', // Capture screenshots on failures
-    video: 'on', // Record video of the test run
     trace: 'on', // Capture execution trace
+    viewport: { width: 1280, height: 720 },
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
   },
 });
